5e7f557e83f93be85bcdbad1f6948176a6a9b031
Erik Gomez <e@erikgomez.co>
2020-05-08 01:12:55 -0700

    Add footer>site-link styles

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -3,6 +3,10 @@ import React from "react"
 import "./footer.scss"
 
 const Footer = () => {
+  const handleSubscribeClick = () => {
+    console.log("Add user")
+  }
+
   return (
     <footer className="footer">
       <div className="opt-in">
@@ -14,7 +18,32 @@ const Footer = () => {
         </p>
         <div className="inputs">
           <input placeholder="Email" type="text" name="email" id="email" />
-          <button>SUBSCRIBE</button>
+          <button onClick={handleSubscribeClick}>SUBSCRIBE</button>
+        </div>
+      </div>
+      <div className="site-links">
+        <div className="tabs">
+          <div className="tab">
+            <input type="checkbox" id="chck1" />
+            <label className="tab-label" htmlFor="chck1">
+              Website
+            </label>
+            <div className="tab-content">
+              <a href="#">Terms</a>
+              <a href="#">Shipping</a>
+              <a href="#">FAQs</a>
+            </div>
+          </div>
+          <div className="tab">
+            <input type="checkbox" id="chck2" />
+            <label className="tab-label" htmlFor="chck2">
+              Info
+            </label>
+            <div className="tab-content">
+              <a href="#">Subscribe</a>
+              <a href="#">Advertise / Contact</a>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
