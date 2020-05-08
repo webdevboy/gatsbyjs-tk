d344f2b1df8623ec908c93743b3912a0e22d2066
Erik Gomez <e@erikgomez.co>
2020-05-07 23:41:32 -0700

    Add footer opt in content

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -2,12 +2,23 @@ import React from "react"
 
 import "./footer.scss"
 
-const Footer = () => (
-  <footer className="footer">
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
-  </footer>
-)
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="opt-in">
+        <h2>Join the inner circle.</h2>
+        <p>
+          Stay updated with the latest news, stories and events. Lorem ipsum
+          dolor etatiti ipsum dolor sit ametait consectetur aditing elit, sed
+          doesnts it eiusmodilai tepo incidiad unt ut labore et dolori.
+        </p>
+        <div className="inputs">
+          <input placeholder="Email" type="text" name="email" id="email" />
+          <button>SUBSCRIBE</button>
+        </div>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
