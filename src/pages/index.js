ffe7ce9fe37ef798d0d0d7453f5578bd1e68b1fa
Erik Gomez <e@erikgomez.co>
2020-05-10 11:54:08 -0700

    Add query for logos. Implement logo component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,66 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "src/components/layout"
+import Logo from "src/components/Logo/logo"
 
-// const ComponentName = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>
-const IndexPage = () => (
-  <Layout>
-    {/* <SEO title="Home" /> */}
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    {/* <Link to="/page-2/">Go to page 2</Link> */}
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  const { homepage } = data.wordpress.pages.edges[0].node
 
-// export const query = graphql`
-//   query RootQueryToPageConnection {
-//     wordpress {
-//       posts {
-//         nodes {
-//           hero {
-//             blockAuthor
-//             blockCopy
-//             blockName
-//             blockTitle
-//             heroBackground {
-//               id
-//               link
-//               srcSet
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+  return (
+    <Layout>
+      {/* <pre>{JSON.stringify(data, null, 4)}</pre> */}
+      <div>
+        <div className="logo-wrap">
+          <Logo theme={"light"} />
+        </div>
+        <img
+          className="hero-bg"
+          src={homepage.hero.sourceUrl}
+          alt={homepage.hero.altText || "Hero background"}
+          style={{
+            width: "100%",
+          }}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query RootQueryToPageConnection {
+    wordpress {
+      pages {
+        edges {
+          node {
+            homepage {
+              hero {
+                sourceUrl
+                id
+                altText
+              }
+              logo {
+                id
+                sourceUrl
+                altText
+              }
+            }
+            themeSelect {
+              fieldGroupName
+              themeSelect
+            }
+          }
+        }
+      }
+      mediaItems {
+        nodes {
+          sourceUrl
+          id
+          title
+          altText
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
