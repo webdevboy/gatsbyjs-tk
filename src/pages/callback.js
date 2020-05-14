82613412e43ea98a17476291b789ea255770ff1e
Erik Gomez <e@erikgomez.co>
2020-05-13 22:29:19 -0700

    Auth testing

--- src/pages/callback.js
+++ src/pages/callback.js
@@ -1,11 +1,24 @@
-// src/pages/callback.js
-import React from "react"
-import { handleAuthentication } from "../utils/auth"
+import React, { useEffect } from "react"
 
-const Callback = () => {
-  handleAuthentication()
+import { useAuth } from "react-use-auth"
+import Layout from "../components/layout"
 
-  return <p>Loading...</p>
+const Auth0CallbackPage = () => {
+  const { handleAuthentication } = useAuth()
+
+  useEffect(() => {
+    // if you want to redirect back to specific page (like the page User comes from?)
+    // handleAuthentication({ postLoginRoute: "/account" });
+    handleAuthentication()
+  }, [])
+
+  return (
+    <Layout>
+      <h1>
+        This is the auth callback page, you should be redirected immediately.
+      </h1>
+    </Layout>
+  )
 }
 
-export default Callback
+export default Auth0CallbackPage
