7a52a79a2f849894b4b8155cf5f5e7c210a6b0c0
Erik Gomez <e@erikgomez.co>
2020-05-18 19:45:10 -0700

    Add hero mobile styles

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+import "./hero.scss"
+
+export default function Hero({ data, theme }) {
+  console.log(data)
+  return (
+    <div className={`hero ${theme}`}>
+      <div className="image-container">
+        <img src={data.heroImage.sourceUrl} alt="" />
+      </div>
+      <div className="block">
+        <p className="name">Destination</p>
+        <h1 className="headline">{data.title}</h1>
+        <p className="byline">{data.byline}</p>
+        <hr />
+        {data.authors.length &&
+          data.authors.split(",").map(author => {
+            return <p className="attribution">{author}</p>
+          })}
+      </div>
+    </div>
+  )
+}
