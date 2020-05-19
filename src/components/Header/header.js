c24b5c42fa053b1d205696d9256f5b9c97757a55
Erik Gomez <e@erikgomez.co>
2020-05-19 01:20:19 -0700

    Add logo link

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,16 +1,22 @@
 import React from "react"
+import { Link } from "gatsby"
 import Logo from "src/svgs/tk_logo"
 import Hamburger from "src/svgs/hamburger"
 
 import "./header.scss"
 
-function Header({ theme }) {
+function Header({ theme, title }) {
   return (
     <header className={`header ${theme}`}>
-      <Logo className="logo" />
+      <Link to="/">
+        <Logo className="logo" />
+      </Link>
       {/* TODO: Implement NAV component */}
       <button onClick={() => console.log("open nav")}>
-        <Hamburger className="hamburger" />
+        <span>
+          <Hamburger className="hamburger" />
+          <p className="title">{title}</p>
+        </span>
       </button>
     </header>
   )
