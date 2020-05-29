2def9248cf9f200ffb24853f8013468f4378b60b
Erik Gomez <e@erikgomez.co>
2020-05-28 21:55:33 -0700

    Remove comment

--- src/components/Quote/quote.js
+++ src/components/Quote/quote.js
@@ -4,7 +4,6 @@ import "./Quote.scss"
 
 export default function Quote({ quote, theme }) {
   return (
-    // <section className="quote">
     <section className={`quote ${theme}`}>
       <blockquote>
         <p>"{quote}"</p>
