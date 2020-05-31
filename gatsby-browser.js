b85fe5e12ad0d6350bc71ef1bac9506686440c7a
Erik Gomez <e@erikgomez.co>
2020-05-31 13:56:47 -0700

    Add auth browser. fix gql query

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,53 +1,32 @@
 // ./gatsby-browser.js
-// import React from "react"
-// import { silentAuth } from "./src/utils/auth"
-//
-// import { navigate } from "gatsby"
-// import { AuthProvider } from "react-use-auth"
+import React from "react"
+import { silentAuth } from "./src/utils/auth"
 
-// class SessionCheck extends React.Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       loading: true,
-//     }
-//   }
+class SessionCheck extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: true,
+    }
+  }
 
-//   handleCheckSession = () => {
-//     this.setState({ loading: false })
-//   }
+  handleCheckSession = () => {
+    this.setState({ loading: false })
+  }
 
-//   componentDidMount() {
-//     silentAuth(this.handleCheckSession)
-//   }
+  componentDidMount() {
+    silentAuth(this.handleCheckSession)
+  }
 
-//   render() {
-//     return (
-//       !this.state.loading && (
-//         <React.Fragment>{this.props.children}</React.Fragment>
-//       )
-//     )
-//   }
-// }
+  render() {
+    return (
+      this.state.loading === false && (
+        <React.Fragment>{this.props.children}</React.Fragment>
+      )
+    )
+  }
+}
 
-// gatsby-browser.js
-
-// import React from "react"
-
-// export const wrapRootElement = ({ element }) => (
-//   <AuthProvider
-//     navigate={navigate}
-//     auth0_domain={process.env.AUTH0_DOMAIN}
-//     auth0_client_id={process.env.AUTH0_CLIENTID}
-//     auth0_params={{
-//       domain: process.env.AUTH0_DOMAIN,
-//       clientID: process.env.AUTH0_CLIENTID,
-//       redirectUri: process.env.AUTH0_CALLBACK,
-//       audience: `https://${process.env.AUTH0_DOMAIN}/api/v2/`,
-//       responseType: "token id_token",
-//       scope: "openid profile email",
-//     }}
-//   >
-//     {element}
-//   </AuthProvider>
-// )
+export const wrapRootElement = ({ element }) => {
+  return <SessionCheck>{element}</SessionCheck>
+}
--- src/components/FiftyFifty/FiftyFifty.postData.js
+++ src/components/FiftyFifty/FiftyFifty.postData.js
@@ -1,17 +1,7 @@
 module.exports = () => {
   return `
     ... on WordPress_Post_Components_Contents_FiftyFifty {
-      backgroundColor
-      copyBlock
       fieldGroupName
-      headline
-      link
-      preHeadline
-      reverse
-      image {
-        sourceUrl
-        altText
-      }
     }
   `
 }
