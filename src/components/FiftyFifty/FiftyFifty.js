dc3c77b71124e7472b8a88a70ad7ccca12536ebc
Erik Gomez <e@erikgomez.co>
2020-05-31 15:27:46 -0700

    Rework grid logic

--- src/components/FiftyFifty/FiftyFifty.js
+++ src/components/FiftyFifty/FiftyFifty.js
@@ -1,11 +1,51 @@
 import React from "react"
+import * as cx from "classnames"
 
 import "./FiftyFifty.scss"
 
-function FiftyFifty(props) {
-  console.log(props)
-
-  return <section className="fifty-fifty">{/* Do work */}</section>
+function FiftyFifty({
+  copyBlock,
+  headline,
+  image,
+  preHeadline,
+  reverse,
+  linkText,
+  linkUrl,
+  copyBackgroundColor,
+  imageBackgroundColor,
+  theme,
+}) {
+  return (
+    <section
+      className={cx({
+        "fifty-fifty": true,
+        [theme]: true,
+        reversed: reverse,
+      })}
+    >
+      <div
+        className="image-block"
+        style={{ backgroundColor: imageBackgroundColor }}
+      >
+        {image.sourceUrl && (
+          <div className="wrapper">
+            <img src={image.sourceUrl} alt={image.alt || "Foodie image"} />
+          </div>
+        )}
+      </div>
+      <div
+        className="copy-block"
+        style={{ backgroundColor: copyBackgroundColor }}
+      >
+        {!reverse && <div className="arrow arrow-up"></div>}
+        <div className="wrapper">
+          <h4>{headline}</h4>
+          <p dangerouslySetInnerHTML={{ __html: copyBlock }}></p>
+        </div>
+        {reverse && <div className="arrow  arrow-down"></div>}
+      </div>
+    </section>
+  )
 }
 
 export default FiftyFifty
--- src/components/FiftyFifty/FiftyFifty.postData.js
+++ src/components/FiftyFifty/FiftyFifty.postData.js
@@ -1,7 +1,19 @@
 module.exports = () => {
   return `
     ... on WordPress_Post_Components_Contents_FiftyFifty {
+      copyBlock
       fieldGroupName
+      headline
+      image {
+        altText
+        sourceUrl
+      }
+      preHeadline
+      reverse
+      linkText
+      linkUrl
+      copyBackgroundColor
+      imageBackgroundColor
     }
   `
 }
