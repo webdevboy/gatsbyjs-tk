41c9fe75bd461234a9d48aec232414037cc20166
Erik Gomez <e@erikgomez.co>
2020-06-03 19:26:27 -0700

    Add theme colors

--- src/svgs/close.js
+++ src/svgs/close.js
@@ -3,8 +3,8 @@ import React from "react"
 export default function Close(props) {
   return (
     <svg {...props} xmlns="http://www.w3.org/2000/svg">
-      <g transform="translate(1 1)" fill="none" fill-rule="evenodd">
-        <g fill="black" stroke="black" strokeWidth="2">
+      <g transform="translate(1 1)" fill-rule="evenodd">
+        <g strokeWidth="2">
           <path d="M7.533 7.65l.453-.442L22.467 21.35l-.453.442z" />
           <path d="M22.467 7.65l-.453-.442L7.533 21.35l.453.442z" />
         </g>
