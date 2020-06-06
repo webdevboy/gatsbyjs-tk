7084b51dab95f3965ea4f4d1fb893cdf877a7dd0
Erik Gomez <e@erikgomez.co>
2020-06-05 23:42:35 -0700

    Add barrel file for icons

---
+++ src/svgs/index.js
@@ -0,0 +1,8 @@
+export { default as Close } from "./close"
+export { default as Facebook } from "./facebook"
+export { default as Hamburger } from "./hamburger"
+export { default as Instagram } from "./instagram"
+export { default as Pinterest } from "./pinterest"
+export { default as Plus } from "./plus"
+export { default as SiteLogo } from "./tk_logo"
+export { default as Twitter } from "./twitter"
