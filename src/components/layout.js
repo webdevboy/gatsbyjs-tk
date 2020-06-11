1536142f738038f54a4178b725d35a4acb09c302
webdevboy <tscriptgem@gmail.com>
2020-06-11 16:57:58 +0300

    remove console.log and test component

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,6 @@ function Layout({ children, theme, title }) {
       />
       <Navigation theme={theme} showNav={showNav} />
       <div>
-        <FullscreenArticle />
         <main>{children}</main>
         <Footer />
       </div>
