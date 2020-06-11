1b51be545f14039c2adcb5f220e75400de64a957
webdevboy <tscriptgem@gmail.com>
2020-06-11 16:57:58 +0300

    remove console.log and test component

--- src/components/FullscreenArticle/FullscreenArticle.js
+++ src/components/FullscreenArticle/FullscreenArticle.js
@@ -24,7 +24,6 @@ function FullscreenArticle({ article, articleInfoPosition }) {
   }
 
   const articleObject = getArticle();
-  console.log(articleObject);
   if(!articleObject) return null;
   return (
     <div className={`fullscreen-article ${articleInfoPosition.toLowerCase().replace(" ", "-")}`} style={{ backgroundImage: articleObject.heroUrl ? `url("${articleObject.heroUrl}")` : '' }}>
