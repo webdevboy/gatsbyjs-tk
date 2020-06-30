46af73cf9ad3e7d66c7af4085c5124933edf4197
Erik Gomez <e@erikgomez.co>
2020-06-30 02:34:37 -0700

    Fix footer style on large breakpoints

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,24 +1,21 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
-import React, { useState } from "react"
-import { useTranslation } from "react-i18next"
-import { Facebook, Pinterest, Twitter, Instagram, SiteLogo } from "src/svgs"
-import { Link } from "@reach/router"
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Facebook, Pinterest, Twitter, Instagram, SiteLogo } from 'src/svgs';
+import { Link } from '@reach/router';
 
 import getLangLink from 'src/utils/getLangLink';
-import "./footer.scss"
+import './footer.scss';
 
 function Footer() {
-  const date = new Date()
+  const date = new Date();
 
   const { t, i18n } = useTranslation('footer');
   return (
     <footer className="footer">
       <div className="opt-in">
         <h2>{t('footer-optin-headline')}</h2>
-        <p>
-          {t('footer-optin-copy')}
-        </p>
-        {/* FIXME: setup to link to sign-up page */}
+        <p>{t('footer-optin-copy')}</p>
         <Link to={getLangLink('/login', i18n.language)}>{t('signup')}</Link>
       </div>
       <div className="site-links">
@@ -67,9 +64,7 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <p className="about">
-          {t('footer-about')}
-        </p>
+        <p className="about">{t('footer-about')}</p>
         <div className="logo">
           <SiteLogo fill="white" />
           <span>TASTING KITCHEN</span>
@@ -79,8 +74,7 @@ function Footer() {
         </p>
       </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
-
+export default Footer;
