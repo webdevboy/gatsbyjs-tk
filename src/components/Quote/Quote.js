970d28e443a0a23f8b46139acf3cc7fd9e0322c3
Erik Gomez <e@erikgomez.co>
2020-06-30 20:52:49 -0700

    Remove hard coded quotes

--- src/components/Quote/Quote.js
+++ src/components/Quote/Quote.js
@@ -1,13 +1,13 @@
-import React from "react"
+import React from 'react';
 
-import "./Quote.scss"
+import './Quote.scss';
 
 export default function Quote({ quote, theme }) {
   return (
     <section className={`quote ${theme}`}>
       <blockquote>
-        <p dangerouslySetInnerHTML={{ __html: `"${quote}"` }}></p>
+        <p dangerouslySetInnerHTML={{ __html: `${quote}` }}></p>
       </blockquote>
     </section>
-  )
+  );
 }
