fd3eddf1996757862b457c6816aadc425ffa9a04
Erik Gomez <e@erikgomez.co>
2020-07-06 20:35:52 -0700

    Add copy column updates

--- src/components/CopyColumns/CopyColumns.js
+++ src/components/CopyColumns/CopyColumns.js
@@ -1,27 +1,50 @@
-import React from "react"
-import { MEDIUM_BREAKPOINT, XLARGE_BREAKPOINT } from "src/utils/breakpoints"
+import React, { useState, useRef } from 'react';
+import * as ScrollMagic from 'scrollmagic';
+import { MEDIUM_BREAKPOINT, XLARGE_BREAKPOINT } from 'src/utils/breakpoints';
+import { isBrowser } from 'src/utils/auth';
 
-import "./CopyColumns.scss"
+import './CopyColumns.scss';
 
 export default function CopyColumns({ columns, theme }) {
-  const cols = 6
+  const [scrollMagic, setScrollMagic] = useState({
+    controller: isBrowser
+      ? new ScrollMagic.Controller({
+          globalSceneOptions: { triggerHook: 'onEnter', duration: '300%' },
+        })
+      : null,
+  });
+
+  const sectionToAnimateRef = useRef(null);
+
+  const cols = 6;
 
   const isBrowser = typeof window !== "undefined"
 
   const getColSpan = () => {
-    if (!isBrowser) return
+    if (!isBrowser) return;
 
     if (
       window.innerWidth >= MEDIUM_BREAKPOINT &&
       window.innerWidth < XLARGE_BREAKPOINT
     ) {
-      return `auto / span ${cols / columns.length}`
+      return `auto / span ${cols / columns.length}`;
     }
 
     if (window.innerWidth >= XLARGE_BREAKPOINT) {
-      return `auto / span 2`
+      return `auto / span 2`;
     }
-  }
+  };
+
+  // const { controller } = scrollMagic;
+  // useEffect(() => {
+  //   new ScrollMagic.Scene({
+  //     triggerElement: sectionToAnimateRef.current,
+  //   })
+  //     .setTween(sectionToAnimateRef.current, { y: '-150%' })
+  //     .addTo(controller);
+  // }, [])
+
+  console.log(columns);
 
   return (
     <section className={`copy-columns ${theme}`}>
@@ -36,7 +59,7 @@ export default function CopyColumns({ columns, theme }) {
               columns.length === 2 &&
               isBrowser &&
               window.innerWidth >= XLARGE_BREAKPOINT
-                ? "2"
+                ? '2'
                 : false,
           }}
         >
@@ -45,12 +68,12 @@ export default function CopyColumns({ columns, theme }) {
               <img
                 className="icon"
                 src={col.icon.sourceUrl}
-                alt={col.altText || "Icon"}
+                alt={col.altText || 'Icon'}
               />
             </span>
           )}
           {col.headline && <h4 className="headline">{col.headline}</h4>}
-          {col.iconSubHeader && <p className="subhead">{col.iconSubHeader}</p>}
+          {col.subheader && <p className="subhead">{col.subheader}</p>}
           <p
             className="copy"
             dangerouslySetInnerHTML={{ __html: col.columnCopy }}
@@ -58,5 +81,5 @@ export default function CopyColumns({ columns, theme }) {
         </div>
       ))}
     </section>
-  )
+  );
 }
