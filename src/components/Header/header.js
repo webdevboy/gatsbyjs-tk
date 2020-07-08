e6c78104c35549db946e0f0cf96d7320359049f3
webdevboy <tscriptgem@gmail.com>
2020-07-08 19:39:56 +0100

    fixed build issue

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -27,11 +27,6 @@ function Hamburger({ isOpen }) {
 }
 
 function ScrollProgressBar({ articleHeaderRef, scrollBlockRef, logoRef, headerOptRef }) {
-  const _document = useDocument();
-  const scrollBlock = null;
-  if(_document) {
-    scrollBlock = _document.querySelector('.page-scroll-wrapper');
-  }
   const _window = useWindow();
   const progressBarRef = useRef();
   const location = useLocation();
