6c7b33b30682f0589e0a36f8685b85c7140f0e46
Erik Gomez <e@erikgomez.co>
2020-07-09 16:55:37 -0700

    Remove Ad Parallax

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -1,35 +1,11 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { Linear } from 'gsap';
-import { isBrowser } from 'src/utils/auth';
-import ScrollMagic from 'scrollmagic';
+import React from 'react';
 
 import './Ad.scss';
 
 function Ad({ mobileAd, tabletAd, desktopAd }) {
-  const imgRef = useRef(null);
-  const [scrollMagic, setScrollMagic] = useState({
-    controller: isBrowser ? new ScrollMagic.Controller() : null,
-  });
-  const { controller } = scrollMagic;
-
   const smallAd = (mobileAd && mobileAd.sourceUrl) || '';
   const mediumAd = (tabletAd && tabletAd.sourceUrl) || '';
   const largeAd = (desktopAd && desktopAd.sourceUrl) || '';
-
-  useEffect(() => {
-    if (!isBrowser) return;
-
-    new ScrollMagic.Scene({
-      duration: '200%',
-      triggerElement: imgRef.current,
-    })
-      .setTween(imgRef.current, {
-        y: '20%',
-        ease: Linear.easeNone,
-        overwrite: 5,
-      })
-      .addTo(controller);
-  }, []);
 
   if (!smallAd || !mediumAd || !largeAd) return null;
 
@@ -41,7 +17,7 @@ function Ad({ mobileAd, tabletAd, desktopAd }) {
             <picture>
               <source srcSet={largeAd} media="(min-width: 1440px)" />
               <source srcSet={mediumAd} media="(min-width: 834px)" />
-              <img className="ad__img" src={smallAd} alt="Ad" ref={imgRef} />
+              <img className="ad__img" src={smallAd} alt="Ad" />
             </picture>
           </div>
         </div>
