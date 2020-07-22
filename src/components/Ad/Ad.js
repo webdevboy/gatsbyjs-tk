b74073e722cd5d6d136db34b7ec228d030f85bad
webdevboy <tscriptgem@gmail.com>
2020-07-22 21:05:11 +0300

    add link to ad component

--- src/components/Ad/Ad.js
+++ src/components/Ad/Ad.js
@@ -2,23 +2,25 @@ import React from 'react';
 
 import './Ad.scss';
 
-function Ad({ mobileAd, tabletAd, desktopAd }) {
+function Ad({ mobileAd, tabletAd, desktopAd, adUrl }) {
   const smallAd = (mobileAd && mobileAd.sourceUrl) || '';
   const mediumAd = (tabletAd && tabletAd.sourceUrl) || '';
   const largeAd = (desktopAd && desktopAd.sourceUrl) || '';
 
   if (!smallAd || !mediumAd || !largeAd) return null;
-
+  console.log(adUrl)
   return (
     <div className="ad">
       <div className="container">
         <div className="ad__img-container">
           <div className="ad__img-wrapper">
-            <picture>
-              <source srcSet={largeAd} media="(min-width: 1440px)" />
-              <source srcSet={mediumAd} media="(min-width: 834px)" />
-              <img className="ad__img" src={smallAd} alt="Ad" />
-            </picture>
+            <a href={adUrl || ''} target="_blank">
+              <picture>
+                <source srcSet={largeAd} media="(min-width: 1440px)" />
+                <source srcSet={mediumAd} media="(min-width: 834px)" />
+                <img className="ad__img" src={smallAd} alt="Ad" />
+              </picture>
+            </a>
           </div>
         </div>
       </div>
