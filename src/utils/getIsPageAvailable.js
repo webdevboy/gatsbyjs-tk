f1ff566cdf74204ecd4c4cd73ffbbce42934d6d7
webdevboy <tscriptgem@gmail.com>
2020-07-29 16:37:47 +0100

    removed console.log in getisPageAvailable

--- src/utils/getIsPageAvailable.js
+++ src/utils/getIsPageAvailable.js
@@ -1,6 +1,5 @@
 const getIsPageAvailable = (pages, url) => {
   if(pages && pages.length > 0) {
-    console.log(pages);
     const page = pages.find(p => {
       const decodedPath = decodeURIComponent(p.path);
       return `${decodedPath}/` === url;
