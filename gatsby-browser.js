ec69f4700ce25d6b99c4eab54d48285783df0c07
webdevboy <tscriptgem@gmail.com>
2020-07-29 15:01:00 +0300

    add padding to not found page, fix language redirect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -32,6 +32,18 @@ class SessionCheck extends React.Component {
     this.setState({ loading: false })
   }
 
+  componentWillMount() {
+    i18next.on('languageChanged', function(lng) {
+      // window.location.pathname = converLinkLocale(globalHistory.location.pathname, lng);
+      window.location.pathname = `/${lng === 'en' ? '' : lng}`;
+    });
+
+    // Set user pref language
+    if(window && globalHistory.location.pathname.indexOf(i18next.language) == -1 && globalHistory.location.pathname.indexOf('callback') == -1 && i18next.language !== 'en') {
+      window.location.pathname = converLinkLocale(globalHistory.location.pathname, i18next.language);
+    }
+  }
+
   componentDidMount() {  
     silentAuth(this.handleCheckSession);
     
@@ -47,16 +59,6 @@ class SessionCheck extends React.Component {
         pulseScale       : 3,
         pulseNormalize   : 1,
       });
-    }
-    i18next.on('languageChanged', function(lng) {
-      
-      // window.location.pathname = converLinkLocale(globalHistory.location.pathname, lng);
-      window.location.pathname = `/${lng === 'en' ? '' : lng}`;
-    });
-
-    // Set user pref language
-    if(window && globalHistory.location.pathname.indexOf(i18next.language) == -1 && globalHistory.location.pathname.indexOf('callback') == -1 && i18next.language !== 'en') {
-      window.location.pathname = converLinkLocale(globalHistory.location.pathname, i18next.language);
     }
   }
   render() {
