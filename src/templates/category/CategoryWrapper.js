f2b4b6bc0b6a1946b93f2ec598f6663c4cc3c771
webdevboy <tscriptgem@gmail.com>
2020-08-01 00:07:58 +0300

    add ampersand replace for post page

--- src/templates/category/CategoryWrapper.js
+++ src/templates/category/CategoryWrapper.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from 'prop-types';
 import { withController } from 'react-scroll-parallax';
 
+import replaceAmpersand from 'src/utils/replaceAmpersand';
 import Layout from "src/components/Layout";
 import SEO from "src/components/seo";
 import CategoryLayouts from "src/components/CategoryLayouts";
@@ -18,7 +19,7 @@ class Category extends Component {
     return (
       <div className="category-page-wrapper">
         <Layout>
-          <SEO title={name ? name.replace('&amp;', '&')  : "Untitled"} />
+          <SEO title={replaceAmpersand(name) || "Untitled"} />
           <CategoryLayouts categoryData={pageContext} updateParallaxState={this.updateParallaxState} />
         </Layout>
       </div>
