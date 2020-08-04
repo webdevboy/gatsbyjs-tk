3123bf1c23a47246770d1298153a5dc99a96f685
webdevboy <tscriptgem@gmail.com>
2020-08-04 19:50:07 +0300

    add instagram posts to socially connected

--- src/components/SociallyConnected/SociallyConnected.js
+++ src/components/SociallyConnected/SociallyConnected.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import Swiper from "react-id-swiper";
-import { navigate } from "gatsby";
+import moment from 'moment';
+import { navigate, useStaticQuery, graphql } from "gatsby";
 import { useTranslation } from "react-i18next";
 
 import { MEDIUM_BREAKPOINT, XLARGE_BREAKPOINT } from "src/utils/breakpoints";
-import { getFormattedArticle } from "src/utils/formatArticle";
 import convertLinkLocale from 'src/utils/convertLinkLocale';
+import Instagram from 'src/images/Instagram_icon_gray.png';
 
 import "../Chefs/Chefs.scss"
 import "./SociallyConnected.scss"
 
 function SociallyConnectedItem({
-  cutline,
+  type,
   title,
   byline,
   imageUrl,
   articleUrl,
+  timestamp,
 }) {
   const [t, i18n] = useTranslation('article');
   return (
-    <div
+    <a
       className="socially__columns__column"
-      onClick={() => {
-        if (articleUrl) {
-          navigate(convertLinkLocale(articleUrl, i18n.language))
-        }
-      }}
+      href={articleUrl}
+      target="_blank"
     >
       {imageUrl && (
         <div className="socially__columns__column__img__wrapper">
@@ -38,15 +37,15 @@ function SociallyConnectedItem({
       )}
 
       <div className="socially__columns__column__info">
-        {cutline && (
-          <div
-            className="socially__columns__column__info_cutline"
-            dangerouslySetInnerHTML={{ __html: cutline }}
-          />
-        )}
+        <div className="socially__columns__column__info__social-type">
+          {type === 'instagram' && <img src={Instagram} alt="instagram" />}
+        </div>
         {title && (
           <div className="socially__columns__column__info_title">{title}</div>
         )}
+        <div className="socially__columns__column__info__social-date">
+          {`POSTED BY TASTING KITCHEN • ${moment(new Date(+`${timestamp}000`)).format('MMMM DD, YYYY')}`}
+        </div>
         {byline && (
           <div className="socially__columns__column__info_description">
             {byline}
@@ -55,16 +54,42 @@ function SociallyConnectedItem({
 
         <div className="socially__columns__column__info_more">
           <span className="socially__columns__column__info_more__link">
-            {t('read-more')}
+            {t('see-post')}
           </span>
         </div>
       </div>
-    </div>
+    </a>
   )
 }
 
 function SociallyConnected({ column1, column2, column3 }) {
   const [t, i18n] = useTranslation('common');
+  const { allInstaNode } = useStaticQuery(graphql`
+    query {
+      allInstaNode {
+        edges {
+          node {
+            likes
+            id
+            username
+            comments
+            original
+            preview
+            caption
+            mediaType
+            thumbnails {
+              config_height
+              config_width
+              src
+            }
+            timestamp
+            type
+          }
+        }
+      }
+    }
+  `);
+  console.log(allInstaNode);
   const params = {
     spaceBetween: 20,
     slidesPerView: 'auto',
@@ -99,13 +124,42 @@ function SociallyConnected({ column1, column2, column3 }) {
     },
   }
 
+  const posts = allInstaNode && allInstaNode.edges && allInstaNode.edges;
+  posts.sort((firstPost, secondPost) => {
+    const firstPostDate = moment(new Date(+`${firstPost.node.timestamp}000`));
+    const secondPostDate = moment(new Date(+`${secondPost.node.timestamp}000`));
+    if(secondPostDate.isBefore(firstPostDate)) {
+      return -1;
+    }
+    else if(firstPostDate.isBefore(secondPostDate)) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <div className="socially-connected">
       <div className="container">
         <div className="socially__title">{t('socially-connected')}</div>
         <div className="socially__columns">
           <Swiper {...params}>
-            {column1 && (
+            {posts.slice(0, 3).map(node => {
+              return (
+                <div key={node.node.id}>
+                  <SociallyConnectedItem
+                    {...{
+                      type: 'instagram',
+                      title: 'From Tasting kitchen Instagram',
+                      byline: node.node.caption,
+                      imageUrl: node.node.original,
+                      articleUrl: `https://www.instagram.com/p/${node.node.id}/`,
+                      timestamp: node.node.timestamp,
+                    }}
+                  />
+                </div>
+              )
+            })}
+            {/* {column1 && (
               <div>
                 <SociallyConnectedItem {...getFormattedArticle(column1)} />
               </div>
@@ -119,7 +173,7 @@ function SociallyConnected({ column1, column2, column3 }) {
               <div>
                 <SociallyConnectedItem {...getFormattedArticle(column3)} />
               </div>
-            )}
+            )} */}
           </Swiper>
         </div>
       </div>
