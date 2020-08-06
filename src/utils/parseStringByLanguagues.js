2bbbd894156740e46a19cc86dea074be42023180
webdevboy <tscriptgem@gmail.com>
2020-08-06 08:15:02 +0100

    updated chineseregxp

--- src/utils/parseStringByLanguagues.js
+++ src/utils/parseStringByLanguagues.js
@@ -2,7 +2,9 @@ const parseStringByLanguagues = (string, callback) => {
   const stringArraybBySpace = string.split(' ');
 
   // Chinese characters range
-  const chineseRegexp = new RegExp("^[\u4E00-\uFA29]*$");
+  // const chineseRegexp = new RegExp("^[\u4E00-\uFA29]*$");
+  const chineseRegexp = /[\u4e00-\u9fff]|[\u3400-\u4dbf]|[\u{20000}-\u{2a6df}]|[\u{2a700}-\u{2b73f}]|[\u{2b740}-\u{2b81f}]|[\u{2b820}-\u{2ceaf}]|[\uf900-\ufaff]|[\u3300-\u33ff]|[\ufe30-\ufe4f]|[\uf900-\ufaff]|[\u{2f800}-\u{2fa1f}]/u;
+  
   let stringArray = [];
   stringArraybBySpace.forEach(element => {
     const splittedElements = element.split('：');
