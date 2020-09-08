4fb704411af7a31b09b2cbf94a012e276bb3a272
webdevboy <tscriptgem@gmail.com>
2020-09-09 00:32:00 +0300

    fix og:url meta tag content

--- src/templates/category/index.js
+++ src/templates/category/index.js
@@ -3,22 +3,22 @@ import { ParallaxProvider } from 'react-scroll-parallax';
 import useWindow from 'src/hooks/useWindow';
 
 import CategoryWrapper from './CategoryWrapper';
+import useWindow from 'src/hooks/useWindow';
 
 const Category = ({ pageContext }) => {
   const _window = useWindow();
-
   const scrollTop = () => {
     const scrollBlock = document.querySelector('.page-scroll');
     const swipeWrapper = document.querySelector('.swipe-wrapper');
     if(!_window) return;
     if(swipeWrapper) {
-      _window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
     else if(scrollBlock) {
-      _window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
     else {
-      _window.scrollTo({ top: 0, behavior: 'smooth' });
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     }
   }
 
