cc275cb7836bfe1b4abb8b5233aa9729fa4a20cf
webdevboy <tscriptgem@gmail.com>
2020-10-15 23:50:59 +0300

    change adaprive image breakpoints

--- src/components/common/AdaptiveImage/AdaptiveImage.js
+++ src/components/common/AdaptiveImage/AdaptiveImage.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 
 import {
   MEDIUM_BREAKPOINT,
-  XLARGE_BREAKPOINT,
+  LARGE_BREAKPOINT,
 } from 'src/utils/breakpoints';
 import useWindow from 'src/hooks/useWindow';
 
 function AdaptiveImage({ src, smallSrc, mediumSrc, innerProps, skeletonActive }) {
   const _window = useWindow();
-  const [width, setWidth] = useState(_window ? _window.innerWidth : XLARGE_BREAKPOINT);
+  const [width, setWidth] = useState(_window ? _window.innerWidth : LARGE_BREAKPOINT);
  
   useEffect(() => {
     let windowResizeListener = null;
@@ -28,10 +28,10 @@ function AdaptiveImage({ src, smallSrc, mediumSrc, innerProps, skeletonActive })
   if(width < MEDIUM_BREAKPOINT && smallSrc) {
     return <img src={smallSrc} alt="" {...innerProps} />
   }
-  else if(width < XLARGE_BREAKPOINT && width > MEDIUM_BREAKPOINT && mediumSrc) {
+  else if(width < LARGE_BREAKPOINT && width > MEDIUM_BREAKPOINT && mediumSrc) {
     return <img src={mediumSrc} alt="" {...innerProps} />
   }
-  else if(width > XLARGE_BREAKPOINT) {
+  else if(width > LARGE_BREAKPOINT) {
     return <img src={src} alt="" {...innerProps} />;
   }
   
